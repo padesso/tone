3beb9337090d52b962ec2e2a88a072c4f1a64e1c
padesso <patrickadesso@gmail.com>
2024-12-20 16:29:53 -0500

    Improved keyboard.

--- app.js
+++ app.js
@@ -135,19 +135,23 @@ async function init() {
 
         // Map keyboard keys to musical notes
         const keyToNote = {
-            'a': 'C4',
-            'w': 'C#4',
-            's': 'D4',
-            'e': 'D#4',
-            'd': 'E4',
-            'f': 'F4',
-            't': 'F#4',
-            'g': 'G4',
-            'y': 'G#4',
-            'h': 'A4',
-            'u': 'A#4',
-            'j': 'B4',
-            'k': 'C5',
+            'q': 'C4',
+            '2': 'C#4',
+            'w': 'D4',
+            '3': 'D#4',
+            'e': 'E4',
+            'r': 'F4',
+            '5': 'F#4',
+            't': 'G4',
+            '6': 'G#4',
+            'y': 'A4',
+            '7': 'A#4',
+            'u': 'B4',
+            'i': 'C5',
+            '9': 'C#5',
+            'o': 'D5',
+            '0': 'D#5',
+            'p': 'E5',
         };
 
         // Track currently playing notes
